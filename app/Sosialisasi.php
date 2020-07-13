1c5d7f6ad4749dd0746dee4b95356f796afdbf21
Thoriq89 <thoriq16@gmail.com>
2020-07-13 20:24:59 +0700

    add tumb di sosialisasi

--- app/Sosialisasi.php
+++ app/Sosialisasi.php
@@ -9,7 +9,7 @@ class Sosialisasi extends Model {
     protected $primaryKey = 'ts_id';
     protected $fillable = [
         'ts_mc_id','ts_nama_kegiatan','ts_tanggal',
-        'ts_user_insert','ts_file1','ts_file2'
+        'ts_user_insert','ts_file1','ts_file2','ts_file1_tumb','ts_file2_tumb'
     ];
     const CREATED_AT = 'ts_date_insert';
     const UPDATED_AT = 'ts_date_update';
