b019a2948137644b7c028be3f299fd69359d38a1
julius <monox.narsis@gmail.com>
2020-10-07 09:49:29 +0700

    modif function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -75,7 +75,7 @@ class DashboardController extends Controller
 	public function getPerimeter_bykategoriperusahaan($name){
 		$datacache =  Cache::remember(env('APP_ENV', 'dev')."_get_perimeter_bykategoriperusahaan2__".$name, 360 * 60, function()use($name){
 	        $data = array();
-	        $perimeter_bykategori_all = DB::select("SELECT * FROM dashboard_perimeterbyperusahaan('$name')");
+	        $perimeter_bykategori_all = DB::select("SELECT * FROM dashboard_perimeterbyperusahaan($name)");
 
 	        foreach($perimeter_bykategori_all as $pka){
 	            $data[] = array(
