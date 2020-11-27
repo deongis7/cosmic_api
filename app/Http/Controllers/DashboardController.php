3db546f1db35a4e081f8f12a43b1df1b4f6bfb69
Thoriq89 <thoriq16@gmail.com>
2020-11-27 08:54:29 +0700

    master

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1217,7 +1217,7 @@ class DashboardController extends Controller
       }
       
       public function getRangkumanAll(){
-          $datacache =  Cache::remember(env('APP_ENV', 'dev')."_get_rangkuman_all", 120 * 60, function() {
+          //$datacache =  Cache::remember(env('APP_ENV', 'dev')."_get_rangkuman_all", 120 * 60, function() {
               $data = array();
               $rangkuman_all = DB::select("SELECT ms_name, mc_name, cnt_mpm, cosmic_index, 
                 cosmic_index_min1, positif, suspek, kontakerat, selesai, 
@@ -1243,6 +1243,6 @@ class DashboardController extends Controller
               }
               return $data;
           });
-          return response()->json(['status' => 200,'data' => $datacache]);
+          //return response()->json(['status' => 200,'data' => $data]);
       }
 }
