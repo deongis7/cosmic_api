414ebf0321bf7187efab2b4f03d0e0753bbe03bc
Thoriq89 <thoriq16@gmail.com>
2021-02-01 14:27:18 +0700

    master

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1524,7 +1524,7 @@ class DashboardController extends Controller
         foreach($vaksin as $itemvaksin){
           $data[] = array(
                   "no" =>  $i++,
-                  "nama" => str_replace("'", '', strval($itemvaksin->tv_nama)),
+                  "nama" => strval($itemvaksin->tv_nama),
                   "status_peg" => $itemvaksin->msp_name2,
                   "jenis_kelamin" => $itemvaksin->tv_mjk_id == 1 ? 'L':'P',
                   "provinsi" => $itemvaksin->mpro_name,
