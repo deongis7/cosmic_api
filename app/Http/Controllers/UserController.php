4dcf97ff3f2c8eca39a939720e89ec9d1d565f95
julius <monox.narsis@gmail.com>
2021-05-19 08:08:48 +0700

    open firebase

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -547,11 +547,11 @@ class UserController extends Controller
         
         
                         //lempar ke helper firebase
-                        /*$token = $get_perimeter[0]->token;
+                        $token = $get_perimeter[0]->token;
                         $body = $get_perimeter[0]->mpml_name."<br /> Field Officer : ". !empty($get_perimeter[0]->first_name)?$get_perimeter[0]->first_name:$get_perimeter[0]->mpml_me_nik;
                         $title = $get_perimeter[0]->mcr_name;
                         $role="FO";
-                        $weeks = AppHelper::sendFirebase($token, $body, $title, $role);*/
+                        $weeks = AppHelper::sendFirebase($token, $body, $title, $role);
         
                         return response()->json(['status' => 200,'message' => 'Data Berhasil Disimpan']);
                     } else {
