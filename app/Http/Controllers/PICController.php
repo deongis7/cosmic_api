8739e64672019cdc7dc22c21d6787833f50a3e31
Julius Jubair <rajawali.king@gmail.com>
2021-09-11 11:17:37 +0700

    tuning API

--- app/Http/Controllers/PICController.php
+++ app/Http/Controllers/PICController.php
@@ -634,7 +634,7 @@ class PICController extends Controller{
 
 	//Get Cluster per Perimeter Level
 	public function getAktifitasbyCluster($nik,$id_perimeter_cluster){
-		$str = 'get_aktifitas_'.$nik.$id_perimeter_cluster;
+		$str = 'get_aktifitas_xx'.$nik.$id_perimeter_cluster;
 		/*$datacache = Cache::tags([$str])->remember(env('APP_ENV', 'prod').$str, 0 * 10, function () use($nik,$id_perimeter_cluster) {*/
         $datacache =Cache::remember(env('APP_ENV', 'prod').$str, 10 * 60, function()use($nik,$id_perimeter_cluster) {    
 		$user = User::where('username',$nik)->first();
