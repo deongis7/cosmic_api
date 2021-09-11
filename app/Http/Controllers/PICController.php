6bb23fba974939a093e8a60a02918f8a8183d244
Julius Jubair <rajawali.king@gmail.com>
2021-09-11 20:41:16 +0700

    tuning query

--- app/Http/Controllers/PICController.php
+++ app/Http/Controllers/PICController.php
@@ -636,7 +636,7 @@ class PICController extends Controller{
 	public function getAktifitasbyCluster($nik,$id_perimeter_cluster){
 		$str = 'get_aktifitas_'.$nik.$id_perimeter_cluster;
 		/*$datacache = Cache::tags([$str])->remember(env('APP_ENV', 'prod').$str, 0 * 10, function () use($nik,$id_perimeter_cluster) {*/
-        $datacache =Cache::remember(env('APP_ENV', 'prod').$str, 10 * 60, function()use($nik,$id_perimeter_cluster) {    
+        $datacache =Cache::remember(env('APP_ENV', 'prod').$str, 0 * 10, function()use($nik,$id_perimeter_cluster) {    
 		$user = User::where('username',$nik)->first();
 		$data = array();
 
