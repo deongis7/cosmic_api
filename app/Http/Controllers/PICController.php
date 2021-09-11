bf8542b4f4d230a915e244fcc30b7847baf61a90
Julius Jubair <rajawali.king@gmail.com>
2021-09-11 16:30:04 +0700

    bugs cluster

--- app/Http/Controllers/PICController.php
+++ app/Http/Controllers/PICController.php
@@ -302,8 +302,8 @@ class PICController extends Controller{
 		$cluster = DB::connection('pgsql2')->select( "select  kc.kcar_id, kc.kcar_mcr_id, kc.kcar_ag_id, mcar.mcar_name,ta.ta_id,ta.ta_status,ta.ta_ket_tolak from konfigurasi_car kc
     		join  master_cluster_ruangan mcr on kc.kcar_mcr_id = mcr.mcr_id
     		join master_car mcar on mcar.mcar_id =kc.kcar_mcar_id and mcar.mcar_active=true
-    		left join transaksi_aktifitas ta on  ta.ta_kcar_id = kc.kcar_id 
-    		where (ta.ta_date >= ? and ta.ta_date <= ? ) and ta.ta_tpmd_id = ? and kc.kcar_mcr_id = ? and kc.kcar_ag_id = 4
+    		left join transaksi_aktifitas ta on  ta.ta_kcar_id = kc.kcar_id and (ta.ta_date >= ? and ta.ta_date <= ? ) and ta.ta_tpmd_id = ?
+    		where  kc.kcar_mcr_id = ? and kc.kcar_ag_id = 4
     		order by mcar.mcar_name asc", [ $startdate, $enddate,$id_perimeter_cluster,$id_cluster]);
 
 		foreach($cluster as $itemcluster){
