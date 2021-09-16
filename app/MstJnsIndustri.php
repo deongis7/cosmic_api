54fa34e17d7a99c7414a1a4774110e168d20cc46
Thoriq89 <thoriq16@gmail.com>
2021-09-16 09:36:00 +0700

    Create MstJnsIndustri.php

---
+++ app/MstJnsIndustri.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class MstJnsIndustri extends Model {
+    protected $table = 'master_jns_industri';
+	protected $primaryKey = 'id';
+	protected $fillable = [
+	    'jenis'
+    ];
+}
