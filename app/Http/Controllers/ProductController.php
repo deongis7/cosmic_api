8761d9186ae6fac5f80f646b6b20eb5066ef39de
Thoriq89 <thoriq16@gmail.com>
2021-10-04 16:49:16 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -599,7 +599,6 @@ class ProductController extends Controller
         $datpl->mppl_mpmk_id = $r_kategori;
         $datpl->mppl_mpro_id = $r_provinsi;
         $datpl->mppl_mkab_id = $r_kabupaten;
-        if
         $datpl->mppl_jml_lantai = $r_lantai;
         $datpl->mppl_kapasitas = $r_kapasitas;
         $datpl->mppl_gmap = $r_maps;
